6c04bac81e9aad9aff6fc9fa0665fc060b4d9c02
daxAKAhackerman <samdeg555@gmail.com>
2020-04-26 13:17:39 -0400

    front end less ugly

--- client/src/components/RenderJSON.vue
+++ client/src/components/RenderJSON.vue
@@ -1,47 +1,59 @@
 <template>
   <b-container fluid>
-    <b-pagination
-      v-model="currentPage"
-      :total-rows="totalRows"
-      :per-page="perPage"
-    ></b-pagination>
-    <b-form-group
-      id="input-group-perPage"
-      label="Number of elements per page: "
-      label-for="input-field-perPage"
-    >
-      <b-form-input id="input-field-perPage" v-model="perPage"></b-form-input>
-    </b-form-group>
     <b-form-group
       id="input-group-size"
       label="Number of elements to fetch: "
       label-for="input-field-size"
     >
-      <b-form-input id="input-field-size" v-model="size"></b-form-input>
-    </b-form-group>
-    <b-form-group id="input-group-button">
-      <b-button variant="primary" id="input-field-button" @click="getData">Fetch</b-button>
-    </b-form-group>
-    <b-form-group label="Filter (supports regex)" label-for="filterInput">
       <b-input-group>
-        <b-form-input v-model="filter" type="search" id="filterInput" placeholder="Type to Search"></b-form-input>
+        <b-form-input id="input-field-size" v-model="size"></b-form-input>
         <b-input-group-append>
-          <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+          <b-button variant="primary" id="input-field-button" @click="getData">Fetch</b-button>
         </b-input-group-append>
       </b-input-group>
     </b-form-group>
-    <b-form-group label="Filter On" description="Leave all unchecked to filter on all data">
-      <b-form-checkbox-group v-model="filterOn">
-        <b-form-checkbox value="timestamp">Timestamp</b-form-checkbox>
-        <b-form-checkbox value="src_ip">Source IP</b-form-checkbox>
-        <b-form-checkbox value="src_port">Source port</b-form-checkbox>
-        <b-form-checkbox value="dest_ip">Destination IP</b-form-checkbox>
-        <b-form-checkbox value="dest_port">Destination port</b-form-checkbox>
-        <b-form-checkbox value="proto">Protocol</b-form-checkbox>
-        <b-form-checkbox value="alert.signature">Signature</b-form-checkbox>
-        <b-form-checkbox value="app_proto">Application protocol</b-form-checkbox>
-      </b-form-checkbox-group>
+
+    <b-form-group id="input-group-buttonFilter">
+      <b-button variant="primary" id="input-field-button" v-b-toggle.filtersCollapse>Filter...</b-button>
     </b-form-group>
+    <b-collapse id="filtersCollapse">
+      <b-card>
+        <b-form-group
+          id="input-group-perPage"
+          label="Number of elements per page: "
+          label-for="input-field-perPage"
+        >
+          <b-form-input id="input-field-perPage" v-model="perPage"></b-form-input>
+        </b-form-group>
+        <b-form-group label="Filter (supports regex)" label-for="filterInput">
+          <b-input-group>
+            <b-form-input
+              v-model="filter"
+              type="search"
+              id="filterInput"
+              placeholder="Type to Search"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+        <b-form-group label="Filter On" description="Leave all unchecked to filter on all data">
+          <b-form-checkbox-group v-model="filterOn">
+            <b-form-checkbox value="timestamp">Timestamp</b-form-checkbox>
+            <b-form-checkbox value="src_ip">Source IP</b-form-checkbox>
+            <b-form-checkbox value="src_port">Source port</b-form-checkbox>
+            <b-form-checkbox value="dest_ip">Destination IP</b-form-checkbox>
+            <b-form-checkbox value="dest_port">Destination port</b-form-checkbox>
+            <b-form-checkbox value="proto">Protocol</b-form-checkbox>
+            <b-form-checkbox value="alert.signature">Signature</b-form-checkbox>
+            <b-form-checkbox value="app_proto">Application protocol</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+      </b-card>
+      <br />
+    </b-collapse>
+    <b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage"></b-pagination>
     <b-table
       :filterIncludedFields="filterOn"
       @filtered="onFiltered"
@@ -59,12 +71,17 @@
         <vue-code-highlight language="json">{{ row.item }}</vue-code-highlight>
       </template>
       <template v-slot:cell(src_ip)="row">
-        <b-link :href="'https://www.virustotal.com/gui/ip-address/' + row.item.src_ip + '/detection'">{{ row.item.src_ip }}</b-link>
+        <b-link
+          :href="'https://www.virustotal.com/gui/ip-address/' + row.item.src_ip + '/detection'"
+        >{{ row.item.src_ip }}</b-link>
       </template>
       <template v-slot:cell(dest_ip)="row">
-        <b-link :href="'https://www.virustotal.com/gui/ip-address/' + row.item.dest_ip + '/detection'">{{ row.item.dest_ip }}</b-link>
+        <b-link
+          :href="'https://www.virustotal.com/gui/ip-address/' + row.item.dest_ip + '/detection'"
+        >{{ row.item.dest_ip }}</b-link>
       </template>
     </b-table>
+    <b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage"></b-pagination>
   </b-container>
 </template>
 
@@ -79,8 +96,8 @@ export default {
   components: {
     VueCodeHighlight
   },
-  mounted () {
-    this.totalRows = this.alerts.length
+  mounted() {
+    this.totalRows = this.alerts.length;
   },
   computed: {
     searchRegex() {
@@ -142,7 +159,7 @@ export default {
     },
     onFiltered(filteredItems) {
       // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
+      this.totalRows = filteredItems.length;
     }
   },
   created() {
